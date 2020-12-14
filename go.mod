a2447a7cf0feffeeba45870a49deade42bccdc25
rin1208 <sora1208sky@gmail.com>
2020-12-14 14:15:45 +0900

    set up

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/gomodule/redigo v1.8.3
+	github.com/jinzhu/gorm v1.9.16
 )
--- main.go
+++ main.go
@@ -5,16 +5,25 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/gomodule/redigo/redis"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func Connection() redis.Conn {
+type Client struct {
+	Redis redis.Conn
+	Mysql *gorm.DB
+}
+
+func init_client() Client {
+	client, _ := gorm.Open("mysql", "root:root@tcp([mysql]:3306)/Weather?charset=utf8mb4&parseTime=true")
+
 	const Addr = "redis:6379"
 
-	c, err := redis.Dial("tcp", Addr)
-	if err != nil {
-		panic(err)
-	}
-	return c
+	c, _ := redis.Dial("tcp", Addr)
+
+	return Client{Mysql: client, Redis: c}
+
 }
 
 func Set(key, value string, c redis.Conn) string {
@@ -33,22 +42,24 @@ func Get(key string, c redis.Conn) string {
 	return res
 }
 
-func hoge(cc *gin.Context) {
-	c := Connection()
-	defer c.Close()
+func (client *Client) hoge(cc *gin.Context) {
+	defer client.Redis.Close()
 
-	res_set := Set("sample-key", "sample-value", c)
+	res_set := Set("sample-key", "sample-value", client.Redis)
 	fmt.Println(res_set) // OK
 
-	res_get := Get("sample-key", c)
+	res_get := Get("sample-key", client.Redis)
 	fmt.Println(res_get) // sample-value
 
 	cc.JSON(200, res_get)
 
 }
 func main() {
+
 	r := gin.Default()
-	// setup()
-	r.GET("/", hoge)
+
+	client := init_client()
+
+	r.GET("/", client.hoge)
 	r.Run() // デフォルトが8080ポートなので今回は変更しません
 }
